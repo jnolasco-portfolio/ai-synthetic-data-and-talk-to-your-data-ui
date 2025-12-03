49140de8264032f890d590232cd94451ea2abc9c
Juan Ruben Nolasco Silva <juan.ruben.nolasco.silva@gmail.com>
2025-12-02 18:34:18 -0600

    Improving loading overlay look

--- src/components/LoadingOverlay.tsx
+++ src/components/LoadingOverlay.tsx
@@ -13,14 +13,14 @@ interface LoadingOverlayProps {
 
 const LoadingOverlay = ({ message, progress }: LoadingOverlayProps) => {
   return (
-    <div className='overlay-container' aria-live='polite'>
+    <dialog open className='overlay-container'>
       <article className='overlay-content'>
         <span aria-busy='true'>{message}</span>
         {progress !== undefined && progress > 0 && (
           <progress value={progress} max='100'></progress>
         )}
       </article>
-    </div>
+    </dialog>
   );
 };
 
